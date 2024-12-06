0b849c21ebafa6621998a3085e9a7ce2a7a60c95
kosukemiura-98 <58726230+kosukemiura-98@users.noreply.github.com>
2024-12-06 18:02:17 +0900

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,67 @@
-$(function () {
-  var thumbSwiper = new Swiper('.thumb-slider', {
-    spaceBetween: 5,
-    slidesPerView: 3,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    breakpoints: {
-      768: {
-        slidesPerView: 5,
-        spaceBetween: 10,
-      }
-    }
-  });
+const mySwiper1 = new Swiper('.swiper1', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination1',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+})
+const mySwiper2 = new Swiper('.swiper2', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    prevEl: '.swiper-button-prev2',
+  },
+})
+const mySwiper3 = new Swiper('.swiper3', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination3',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next3',
+    prevEl: '.swiper-button-prev3',
+  },
+})
 
-  var mainSwiper = new Swiper('.main-slider', {
-    spaceBetween: 5,
-    slidesPerView: 1.1,
-    centeredSlides: true,
-    loop: true,
-    loopedSlides: 3,
-    autoplay: {
-      delay: 3000
-    },
-    speed: 1500,
-    thumbs: {
-      swiper: thumbSwiper
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 1.3,
-        spaceBetween: 10,
-      }
-    }
-  });
+$('.feature__header').click(function () {
+  $(this).next().slideToggle();
+  $(this).toggleClass('is-open');
+  if ($(this).hasClass("is-open")) {
+    $(this).text("詳細を閉じる");
+  } else {
+    $(this).text("詳細を開く");
+  }
+});
 
+var trigger = $('.modal__trigger'),
+  wrapper = $('.modal__wrapper'),
+  layer = $('.modal__layer'),
+  container = $('.modal__container'),
+  close = $('.modal__close'),
+  content = $('.modal__content');
+
+$(trigger).click(function () {
+  $(wrapper).fadeIn(400);
+  $(content).html($(this).prop('outerHTML'));
+  $(container).scrollTop(0);
+  $('html, body').css('overflow', 'hidden');
+});
+
+$(layer).add(close).click(function () {
+  $(wrapper).fadeOut(400);
+  $('html, body').removeAttr('style');
+});
+
+$('.qa__header').click(function () {
+  $(this).next().slideToggle();
+  $(this).toggleClass('is-open');
 });
